abb84cd6cde3c8edd75a06da07eaa19357e492d5
Carlose <carlosviniwork@gmail.comm>
2021-07-11 02:24:14 -0300

    Inserção da Funcionalidade do bot

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function recebeNumero(pm){
 function clean(){
     document.getElementById('resultado').innerHTML = "";
 }
+function cancelEntry(){
+    let searchLastEntry = document.getElementById('resultado').innerHTML;
+    let lastEntry = searchLastEntry.length -1;
+    while (searchLastEntry[lastEntry] != '+' || searchLastEntry != '-' || searchLastEntry != '/' || searchLastEntry != '*') {
+        if (lastEntry > 0) {
+            lastEntry--
+        }
+        break
+    }
+    document.getElementById('resultado').innerHTML = searchLastEntry.substring(0, lastEntry);
+}
 function back(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
